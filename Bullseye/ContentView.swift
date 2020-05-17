778fea9573dbefa998d546abb21c64a75a62b746
nuginurghani <nughie28@gmail.com>
2020-05-18 06:13:40 +0700

    Update ContentView.swift

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,20 +9,104 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-      VStack {
-        Text("Welcome to my first app!")
-          .fontWeight(.semibold)
-          .foregroundColor(Color.green)
+
+  @State var alertIsVisible = false
+  @State var sliderValue = 50.0
+  @State var target = Int.random(in: 1...100)
+  @State var score = 0
+  @State var round = 1
+ 
+  var body: some View {
+    VStack {
+      Spacer()
+      
+      // Target row
+      HStack {
+        Text("Put the bullseye as close as you can to:")
+        Text("\(target)")
+      }
+      Spacer()
+      
+      // Slider row
+      HStack {
+        Text("1")
+        Slider(value: $sliderValue, in: 1...100)
+        Text("100")
+      }
+      Spacer()
+      
+      // Button row
+      Button(action: {
+        print("Button pressed!")
+        self.alertIsVisible = true
+        
+      }) {
+        Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+      }
+      .alert(isPresented: $alertIsVisible) { () -> Alert in
+        //let roundedValue = Int(sliderValue.rounded())
+        return Alert(title: Text(alertTitle()), message: Text(
+          "The slider's value is \(sliderValueRounded()).\n" +
+          "You scored \(pointsForCurrentRound()) points this round."
+        ), dismissButton: .default(Text("Awesome!")) {
+          self.score = self.score + self.pointsForCurrentRound()
+          self.target = Int.random(in: 1...100)
+          self.round = self.round + 1
+        })
+      }
+      Spacer()
+      
+      // Score row
+      HStack {
         Button(action: {}) {
-          Text("Hit Me!")
+          Text("Start Over")
+        }
+        Spacer()
+        Text("Score:")
+        Text("\(score)")
+        Spacer()
+        Text("Round:")
+        Text("\(round)")
+        Spacer()
+        Button(action: {}) {
+          Text("Info")
         }
       }
+      .padding(.bottom, 20)
     }
+  }
+  
+  func sliderValueRounded() -> Int {
+    Int(sliderValue.rounded())
+  }
+  
+  func amountOff() -> Int {
+    abs(target - sliderValueRounded())
+  }
+  
+  func pointsForCurrentRound() -> Int {
+    100 - amountOff()
+  }
+  
+  func alertTitle() -> String {
+    let difference = amountOff()
+    let title: String
+    if difference == 0 {
+      title = "Perfect!"
+    } else if difference < 5 {
+      title = "You almost had it!"
+    } else if difference <= 10 {
+      title = "Not bad."
+    } else {
+      title = "Are you even trying?"
+    }
+    return title
+  }
+  
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView().previewLayout(.fixed(width: 896, height: 414))
+  }
 }
