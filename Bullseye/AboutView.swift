7da49fec724bf6cf6df5410f7945f817924f433f
nuginurghani <nughie28@gmail.com>
2020-05-18 14:51:50 +0700

    Update AboutView.swift

--- Bullseye/AboutView.swift
+++ Bullseye/AboutView.swift
@@ -9,14 +9,42 @@
 import SwiftUI
 
 struct AboutView: View {
+    
+    let beige = Color(red: 255.0/255.0, green: 214.0/255.0, blue: 179.0/255.0)
+    
+    struct AboutHeadingStyle: ViewModifier {
+      func body(content: Content) -> some View {
+        return content
+            .foregroundColor(Color.black)
+            .font(Font.custom("Arial Rounded MT Bold", size: 30))
+            .padding(.top,20)
+            .padding(.bottom,20)
+      }
+    }
+    
+    struct AboutBodyStyle: ViewModifier {
+      func body(content: Content) -> some View {
+        return content
+            .foregroundColor(Color.black)
+            .font(Font.custom("Arial Rounded MT Bold", size: 16))
+            .padding(.leading,60)
+            .padding(.trailing,60)
+            .padding(.bottom,20)
+      }
+    }
+    
     var body: some View {
-        VStack{
-            Text("🎯 Bullseye 🎯")
-            Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider")
-            Text("Your goal is to place the slider as close as possible to the target value. The closer you are, the more points you score").multilineTextAlignment(.center)
-            Text("Enjoy!")
+        Group{
+            VStack{
+                Text("🎯 Bullseye 🎯").modifier(AboutHeadingStyle())
+                Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider").modifier(AboutBodyStyle()).multilineTextAlignment(.center)
+                Text("Your goal is to place the slider as close as possible to the target value. The closer you are, the more points you score").modifier(AboutBodyStyle()).multilineTextAlignment(.center)
+                Text("Enjoy!").modifier(AboutBodyStyle())
+                }
+            .navigationBarTitle("About Bullseye")
+            .background(beige)
         }
-    .navigationBarTitle("About Bullseye")
+    .background(Image("Background"))
     }
 }
 
